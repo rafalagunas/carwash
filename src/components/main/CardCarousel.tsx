6a526227780b48cf086020226bcdcd60b25a9f14
rafalagunas <guitronleafar@gmail.com>
2024-08-13 18:06:16 -0600

    updated forms

--- src/components/main/CardCarousel.tsx
+++ src/components/main/CardCarousel.tsx
@@ -17,6 +17,7 @@ import img1 from "../../assets/main/1.png";
 import img2 from "../../assets/main/2.png";
 import img3 from "../../assets/main/3.png";
 import img4 from "../../assets/main/4.png";
+import img5 from "../../assets/main/5.png";
 
 import soap from "../../assets/svg/soap-solid.svg";
 import shapoo from "../../assets/svg/jug-detergent-solid.svg";
@@ -24,7 +25,7 @@ import wheels from "../../assets/svg/wheel.svg";
 import scent from "../../assets/svg/spray-can-sparkles-solid.svg";
 import vacuum from "../../assets/svg/vacuum.svg";
 import rug from "../../assets/svg/rug-solid.svg";
-import car from "../../assets/svg/car-solid.svg";
+
 import chemicalGuys from "../../assets/svg/chemical-guys.svg";
 import purplePower from "../../assets/svg/purple-power.svg";
 import rainX from "../../assets/svg/rain-x.svg";
@@ -68,46 +69,42 @@ const CardCarousel = () => {
   const services = [
     {
       id: 1,
-      title: "Servicio Express",
+      title: "Servicio Bronce",
       imgSrc: img1,
       services: [
         { icon: soap, label: "Exterior" },
         { icon: shapoo, label: "Shampoo" },
-        { icon: wheels, label: "Rines" },
+        { icon: vacuum, label: "Aspirado de tapetes, pisos y cajuelas" },
+        {
+          icon: null,
+          label:
+            "** Si el auto cuenta con sueciedad EXTREMA, se aplica un costo extra",
+        },
       ],
     },
     {
       id: 2,
-      title: "Servicio Premium",
+      title: "Servicio Plata",
       imgSrc: img2,
       services: [
         { icon: soap, label: "Exterior" },
         { icon: shapoo, label: "Shampoo" },
         { icon: wheels, label: "Rines" },
-        { icon: car, label: "Interiores" },
-        { icon: vacuum, label: "Aspirado" },
+        { icon: vacuum, label: "Aspirado de tapetes, pisos y cajuela" },
         { icon: rug, label: "Lavado de tapetes" },
+        { icon: shapoo, label: "Cera" },
+        { icon: scent, label: "Aroma" },
+        {
+          icon: null,
+          label:
+            "** Si el auto cuenta con sueciedad EXTREMA, se aplica un costo extra",
+        },
       ],
     },
     {
       id: 3,
-      title: "Servicio Platino",
+      title: "Servicio Oro",
       imgSrc: img3,
-      services: [
-        { icon: soap, label: "Exterior" },
-        { icon: shapoo, label: "Shampoo" },
-        { icon: wheels, label: "Rines" },
-        { icon: car, label: "Interiores" },
-        { icon: vacuum, label: "Aspirado" },
-        { icon: rug, label: "Lavado de tapetes" },
-        { icon: shapoo, label: "Cera" },
-        { icon: scent, label: "Aroma" },
-      ],
-    },
-    {
-      id: 4,
-      title: "Servicio Platino Plus",
-      imgSrc: img4,
       services: [
         {
           icon: soap,
@@ -120,25 +117,66 @@ const CardCarousel = () => {
             "Shampoo hiperconcentrado a base de cítricos y potenciador del brillo formulado para resaltar un brillo vibrante en todos los colores de pintura",
         },
         { icon: purplePower, label: "Limpieza profunda de rines y llantas" },
-        { icon: vacuum, label: "Aspirado" },
+        {
+          icon: vacuum,
+          label: "Aspirado de tapetes, asientos, pisos y cajuela",
+        },
         { icon: purplePower, label: "Lavado y desengrasante de tapetes" },
         {
           icon: meguiars,
           label: "Humectante plásticos y pieles de interiores",
         },
         { icon: chemicalGuys, label: "Cera de carnauba" },
+
+        { icon: rainX, label: "Limpiador de vidrios repelente al agua" },
+        { icon: scent, label: "Aroma premium a elección" },
+        {
+          icon: null,
+          label:
+            "** Si el auto cuenta con sueciedad EXTREMA, se aplica un costo extra",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Servicio Diamante",
+      imgSrc: img4,
+      services: [
+        { icon: null, label: "Lo mismo que Servicio Oro pero con: " },
+        {
+          icon: chemicalGuys,
+          label:
+            "Descontaminación de pintura para retirar materiales incrustados y adheridos a la pintura para dejar liso el coche",
+        },
+        {
+          icon: chemicalGuys,
+          label: "Rejuvenecimiento y renovación de pieles y vinilos",
+        },
+        {
+          icon: chemicalGuys,
+          label:
+            "Limpieza a PROFUNDIDAD de hendiduras de puertas, tableros y plásticos en general para una renovación interior completa.",
+        },
+        { icon: rainX, label: "Líquido para vidrios anti-empañante" },
+        {
+          icon: chemicalGuys,
+          label: "Fijador y exponenciador de cera para un auto como espejo",
+        },
         { icon: chemicalGuys, label: "Restaurador de llantas" },
         {
           icon: chemicalGuys,
-          label: "Limpieza de vestiduras y cielo superficial",
+          label:
+            "Cerámica hidrofóbica para la carrocería, no más manchas de agua.",
         },
-        {
-          icon: chemicalGuys,
-          label: "Líquido restaurador de molduras, plásticos y vinilos",
-        },
-        { icon: rainX, label: "Líquido para vidrios anti-empañante" },
-        { icon: rainX, label: "Limpiador de vidrios repelente al agua" },
-        { icon: scent, label: "Aroma premium a elección" },
+      ],
+    },
+    {
+      id: 5,
+      title: "Servicio Diamante con Faros",
+      imgSrc: img5,
+      services: [
+        { icon: null, label: "Lo mismo que Servicio Diamante pero con: " },
+        { icon: chemicalGuys, label: "Pulido de faros delanteros " },
       ],
     },
   ];
@@ -159,7 +197,7 @@ const CardCarousel = () => {
             spaceBetween: 20,
           },
           1024: {
-            slidesPerView: 4,
+            slidesPerView: 5,
             spaceBetween: 30,
           },
         }}
@@ -179,12 +217,16 @@ const CardCarousel = () => {
                 <div className="sub__services">
                   {service.services.map((item, index) => (
                     <div key={index} className="items">
-                      <img
-                        width="25"
-                        height="25"
-                        src={item.icon}
-                        alt={item.label}
-                      />
+                      {item.icon != null ? (
+                        <img
+                          width="25"
+                          height="25"
+                          src={item.icon}
+                          alt={item.label}
+                        />
+                      ) : (
+                        <></>
+                      )}
                       <p>{item.label}</p>
                     </div>
                   ))}
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,83 +1,138 @@
-import React from 'react'
-import Layout from './Layout'
-import CardCarousel from './CardCarousel'
-import Form from './Form'
+import React from "react";
+import Layout from "./Layout";
+import CardCarousel from "./CardCarousel";
+import Form from "./Form";
 
-import drop from '../../assets/svg/droplet-solid.svg';
-
-import './Main.css'
-
+import "./Main.css";
 
 const Main: React.FC = () => {
   return (
-    <div className='main'>
+    <div className="main">
       <Layout />
-      <div className='row__possession'>
-        <div className='title__possession'>
-            <p>Todos nuestros servicios cuentan con seguro para lluvia <img src={drop} width={15} height={15} />. <br/> Si llueve y se te ensucia, lo lavamos gratis dentro de las 24 horas siguientes del lavado</p>
+      <div className="row__possession">
+        <div className="title__possession">
+          <p>
+            Todos nuestros productos son específicamente para cada parte de tu
+            vehículo, por lo que recibirás un tratamiento especial y de calidad
+            asegurada.
+            <br /> No usamos productos comunes o convencionales como en
+            cualquier lavacoches, nosotros queremos asegurar la calidad de
+            nuestro servicio. <br />
+            <br />
+            Si vas a un lugar en donde le ponen ArmorAll a tus plásticos, no
+            cuidan tu coche.
+          </p>
         </div>
       </div>
-      <div className='row__two'>
-        <div className='our__title_container'>
-            <p>
-                <span className='our__title'>Si tienes el auto sucio y necesitas una limpieza pero no tienes tiempo de llevarlo a lavar</span> somos la mejor opción para ti, ofreciéndote la mejor calidad gracias a que usamos los mejores productos del mercado para cuidar tu coche y puntualidad porque sabemos que tu tiempo es lo más importante.
-            </p>
+      <div className="row__two">
+        <div className="our__title_container">
+          <p>
+            <span className="our__title">
+              Si tienes el auto sucio y necesitas una limpieza pero no tienes
+              tiempo de llevarlo a lavar
+            </span>{" "}
+            somos la mejor opción para ti, ofreciéndote la mejor calidad gracias
+            a que usamos los mejores productos del mercado para cuidar tu coche
+            y puntualidad porque sabemos que tu tiempo es lo más importante.
+          </p>
         </div>
-        
       </div>
-      <div className='row__four'>
-            <div>
-                <div className='title__container_our_services'>
-                    <p className='title'>Nuestros servicios</p>
-                </div>
-                <div className='item__service_container'>
-                    <div className='item__service'>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width='25' viewBox="0 0 512 512"><path fill="#63E6BE" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>
-                        </div>
-                        Calidad
-                    </div>
-                    <div className='item__service'>
-                        <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width='25' viewBox="0 0 512 512"><path fill="#74C0FC" d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                        </div>
-                        Tiempo
-                    </div>
-                    <div className='item__service'>
-                        <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width='28' fill='1d3557' viewBox="0 0 576 512"><path d="M566.6 54.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192-34.7-34.7c-4.2-4.2-10-6.6-16-6.6c-12.5 0-22.6 10.1-22.6 22.6l0 29.1L364.3 320l29.1 0c12.5 0 22.6-10.1 22.6-22.6c0-6-2.4-11.8-6.6-16l-34.7-34.7 192-192zM341.1 353.4L222.6 234.9c-42.7-3.7-85.2 11.7-115.8 42.3l-8 8C76.5 307.5 64 337.7 64 369.2c0 6.8 7.1 11.2 13.2 8.2l51.1-25.5c5-2.5 9.5 4.1 5.4 7.9L7.3 473.4C2.7 477.6 0 483.6 0 489.9C0 502.1 9.9 512 22.1 512l173.3 0c38.8 0 75.9-15.4 103.4-42.8c30.6-30.6 45.9-73.1 42.3-115.8z"/></svg>
-                        </div>
-                        Limpieza
-                    </div>
-                    <div className='item__service'>
-                        <div>
-                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-premium-rights"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M13.867 9.75c-.246 -.48 -.708 -.769 -1.2 -.75h-1.334c-.736 0 -1.333 .67 -1.333 1.5c0 .827 .597 1.499 1.333 1.499h1.334c.736 0 1.333 .671 1.333 1.5c0 .828 -.597 1.499 -1.333 1.499h-1.334c-.492 .019 -.954 -.27 -1.2 -.75" /><path d="M12 7v2" /><path d="M12 15v2" /></svg>
-                        </div>
-                        Precio
-                    </div>
-                </div>
+      <div className="row__four">
+        <div>
+          <div className="title__container_our_services">
+            <p className="title">Nuestros servicios</p>
+          </div>
+          <div className="item__service_container">
+            <div className="item__service">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="#63E6BE"
+                    d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"
+                  />
+                </svg>
+              </div>
+              Calidad
             </div>
-    </div>
-    <div className='row__three'>
+            <div className="item__service">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="#74C0FC"
+                    d="M256 0a256 256 0 1 1 0 512A256 256 0 1 1 256 0zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+                  />
+                </svg>
+              </div>
+              Tiempo
+            </div>
+            <div className="item__service">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  fill="1d3557"
+                  viewBox="0 0 576 512"
+                >
+                  <path d="M566.6 54.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192-34.7-34.7c-4.2-4.2-10-6.6-16-6.6c-12.5 0-22.6 10.1-22.6 22.6l0 29.1L364.3 320l29.1 0c12.5 0 22.6-10.1 22.6-22.6c0-6-2.4-11.8-6.6-16l-34.7-34.7 192-192zM341.1 353.4L222.6 234.9c-42.7-3.7-85.2 11.7-115.8 42.3l-8 8C76.5 307.5 64 337.7 64 369.2c0 6.8 7.1 11.2 13.2 8.2l51.1-25.5c5-2.5 9.5 4.1 5.4 7.9L7.3 473.4C2.7 477.6 0 483.6 0 489.9C0 502.1 9.9 512 22.1 512l173.3 0c38.8 0 75.9-15.4 103.4-42.8c30.6-30.6 45.9-73.1 42.3-115.8z" />
+                </svg>
+              </div>
+              Limpieza
+            </div>
+            <div className="item__service">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-premium-rights"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                  <path d="M13.867 9.75c-.246 -.48 -.708 -.769 -1.2 -.75h-1.334c-.736 0 -1.333 .67 -1.333 1.5c0 .827 .597 1.499 1.333 1.499h1.334c.736 0 1.333 .671 1.333 1.5c0 .828 -.597 1.499 -1.333 1.499h-1.334c-.492 .019 -.954 -.27 -1.2 -.75" />
+                  <path d="M12 7v2" />
+                  <path d="M12 15v2" />
+                </svg>
+              </div>
+              Precio
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row__three">
         <CardCarousel />
         {/* <div className='title__container_services__carwash'>
             <p className='title'>Nustrso servicios</p>
         </div> */}
-        <div className='services__carwash'>
-            
-            
-        </div>
-    </div>
-    <div className='row__possession'>
-        <div className='title__possession'>
-            <p>Recuerda al finalizar tu lavado, pedir tu tarjeta de cliente frecuente en donde después de la séptima limpieza, te regalaremos un lavado platino COMPLETAMENTE GRATIS.</p>
+        <div className="services__carwash"></div>
+      </div>
+      <div className="row__possession">
+        <div className="title__possession">
+          <p>
+            Recuerda al finalizar tu lavado, pedir tu tarjeta de cliente
+            frecuente en donde después de la séptima limpieza, te regalaremos un
+            lavado platino COMPLETAMENTE GRATIS.
+          </p>
         </div>
       </div>
-    <div className='row__five'>
+      <div className="row__five">
         <Form />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
